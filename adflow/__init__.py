20a8cb786f56e23f95784bc04474cedce68237c2
Andrew Lamkin <lamkina@umich.edu>
2024-06-25 11:09:08 -0400

    Adding slice module to the adflow init file

--- adflow/__init__.py
+++ adflow/__init__.py
@@ -2,7 +2,8 @@ __version__ = "2.10.0"
 
 from mpi4py import MPI
 
+from .checkZipper import checkZipper
+from .oversetCheck import OversetCheck
 from .pyADflow import ADFLOW
 from .pyADflow_C import ADFLOW_C
-from .oversetCheck import OversetCheck
-from .checkZipper import checkZipper
+from .slicesPost import SliceInterface
